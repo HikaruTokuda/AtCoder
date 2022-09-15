67c269edfc9e62cff1aa79ee98b3c9f9787a8e0f
CO\MM13406 <hikaru.tokuda@murata.com>
2022-09-15 15:27:20 +0900

    プロジェクト作成

---
+++ AtCoderKyoupro90/AtCoderKyoupro90/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtCoderKyoupro90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
